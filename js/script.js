ba5a8da97835371e95f4ab0f36fc0c4b8877b742
Deepak Pandey <d.pandey11585@gmail.com>
2021-06-23 07:17:51 +0530

    Project-Portfolio | Version:1.0 Message:Initial Draft

---
+++ js/script.js
@@ -0,0 +1,34 @@
+
+function ShowAndHide(x) {
+
+    if(x == 'education'){
+        if(document.getElementById('timeline-education').style.display == 'block'){
+            document.getElementById('timeline-education').style.display='none';
+        }
+    
+        else if(document.getElementById('timeline-education').style.display == 'none'){
+            document.getElementById('timeline-education').style.display='block';
+        }
+    }
+    
+}
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+var navbar = document.getElementById("navbar");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+
