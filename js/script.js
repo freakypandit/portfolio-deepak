d8c6cabe999dc835191d85858c8769304894d8da
Deepak Pandey <d.pandey11585@gmail.com>
2021-06-25 12:11:21 +0530

    Project section Added

--- js/script.js
+++ js/script.js
@@ -2,15 +2,35 @@
 function ShowAndHide(x) {
 
     if(x == 'education'){
-        if(document.getElementById('timeline-education').style.display == 'block'){
-            document.getElementById('timeline-education').style.display='none';
+        if(document.getElementById('timeline-education').style.display == 'block' 
+            && document.getElementById('timeline-experience').style.display == 'none'){
+            
+              document.getElementById('timeline-education').style.display='block';
         }
-    
-        else if(document.getElementById('timeline-education').style.display == 'none'){
-            document.getElementById('timeline-education').style.display='block';
+
+        else if(document.getElementById('timeline-experience').style.display == 'block' 
+            && document.getElementById('timeline-education').style.display == 'none'){
+              
+              document.getElementById('timeline-experience').style.display='none';
+              document.getElementById('timeline-education').style.display='block';
+              
         }
     }
-    
+
+    else if(x == 'experience'){
+      if(document.getElementById('timeline-education').style.display == 'block' 
+          && document.getElementById('timeline-experience').style.display == 'none'){
+          
+            document.getElementById('timeline-experience').style.display='block';
+            document.getElementById('timeline-education').style.display='none';
+      }
+
+      else if(document.getElementById('timeline-experience').style.display == 'block' 
+          && document.getElementById('timeline-education').style.display == 'none'){
+          
+            document.getElementById('timeline-experience').style.display='block';
+      }
+    }
 }
 
 // When the user scrolls the page, execute myFunction
@@ -24,11 +44,14 @@ var sticky = navbar.offsetTop;
 
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
-  if (window.pageYOffset >= sticky) {
+  if (window.
+    pageYOffset >= sticky) {
     navbar.classList.add("sticky")
   } else {
     navbar.classList.remove("sticky");
   }
 }
+
+
 
 
